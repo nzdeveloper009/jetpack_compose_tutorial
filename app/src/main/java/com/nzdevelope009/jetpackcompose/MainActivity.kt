916c38cb06110a2c7e15048731f933a876fd4bbe
nzdeveloper009 <nzdeveloper009@gmail.com>
2023-04-01 13:11:28 +0500

    Layout in Jetpack Compose - Column - Row - Box - Example of ListViewItem

--- app/src/main/java/com/nzdevelope009/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/nzdevelope009/jetpackcompose/MainActivity.kt
@@ -5,12 +5,10 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
@@ -21,6 +19,7 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nzdevelope009.jetpackcompose.ui.theme.JetpackComposeTheme
 
@@ -84,8 +83,99 @@ private fun inputCompose() {
             state.value = it
             Log.d("NZDeveloper009_InputCompose", "inputCompose -> $it")
         },
-        label = { Text(text = "Enter Name") }
+        label = {
+            Text(
+                text = "Enter Name"
+            )
+        }
     )
+}
+
+/*
+* Layout Composables - Row, Column, Box
+* Arragnment = Bacha howa space elements k bech kese divide hoga
+* Alignment = rakhna kese hai elements ko
+* */
+@Composable
+private fun colCompose() {
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "A",
+            fontSize = 24.sp
+        )
+        Text(
+            text = "B",
+            fontSize = 24.sp
+        )
+    }
+}
+
+@Composable
+private fun rowCompose() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "A",
+            fontSize = 24.sp
+        )
+        Text(
+            text = "B",
+            fontSize = 24.sp
+        )
+    }
+}
+
+/*
+* Box similar like FrameLayout
+* in box we use contentAlignment attribute to align element
+*
+* */
+@Composable
+private fun boxCompose() {
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(
+                id = R.drawable.ic_broken_heart
+            ),
+            contentDescription = "Broken Heart"
+        )
+        Image(
+            painter = painterResource(
+                id = R.drawable.ic_arrow
+            ),
+            contentDescription = "Arrow"
+        )
+    }
+}
+
+/*
+* Example of layout
+* */
+
+@Composable
+private fun ListViewItem() {
+    Row {
+        Image(
+            painter = painterResource(
+                id = R.drawable.ic_person_pin
+            ),
+            contentDescription = "Person Pin",
+        )
+        Column() {
+            Text(
+                text = "Syed Nokhaiz Al Hassan",
+                fontWeight = FontWeight.Thin,
+                fontSize = 12.sp
+            )
+        }
+    }
 }
 
 @Preview(
@@ -97,5 +187,9 @@ private fun inputCompose() {
 private fun previewFunction() {
 //    sayDevs("Nokhaiz")
 //    imgFun()
-    inputCompose()
+//    inputCompose()
+//    colCompose()
+//    rowCompose()
+//    boxCompose()
+    ListViewItem()
 }
